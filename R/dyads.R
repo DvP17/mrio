4887d0017318dbce0a873c9e9b34e04ff4760b51
David Presberger <david.presberger@stud.sbg.ac.at>
2019-04-10 22:30:12 +0200

    added a country argument to dyads function

--- R/dyads.R
+++ R/dyads.R
@@ -6,17 +6,18 @@
 #' Note that high RAM is required to execute this function for Exiobase and
 #' multiple years.
 #'
-#' @usage dyads(year, data)
+#' @usage dyads(year, data, country)
 #'
 #' @param year Numeric vector or number for the respective year/s
 #' @param data Data object/ matrix that was created before
+#' @param country String or character vector for corresponding country/ies
 #'
 #' @return None
 #'
 #' @examples dyads(year = 1995:1996, eoramatrixobject)
 #'
 #' @export
-dyads <- function(year, data) {
+dyads <- function(year, data, country) {
 
   # Evaluate data input
   if (!is.list(data)) {
@@ -32,25 +33,39 @@ dyads <- function(year, data) {
   else {cat("Data input must be matrix or list.")
   }
 
-
   # Main
   if (type == "eoramatrix") {
-    eoralabels <- read.delim(paste0("Eora26_", min(year), "_bp/labels_T.txt"),
-                             header = F)
-    colnames(data) <- c(paste0(eoralabels[[2]], "_", eoralabels[[4]]))
+    labels <- read.delim(paste0("Eora26_", min(year), "_bp/labels_T.txt"),
+                         header = F)
+
+    if (hasArg(country)) {
+      data <- data[which(labels$V2 %in% country), which(labels$V2 %in% country)]
+      labels <- labels[which(labels$V2 %in% country),]
+    }
+
+    colnames(data) <- c(paste0(labels[[2]], "_", labels[[4]]))
     row.names(data) <- colnames(data)
 
     data <- reshape2::melt(data, as.is = T) # as.is makes chr
     data <- tidyr::separate(data, Var1, into = c("C1", "S1"), sep = "_")
     data <- tidyr::separate(data, Var2, into = c("C2", "S2"), sep = "_")
 
+    # data <- data[order(data$C1),]
     final <- cbind(year, data)
+
   }
 
   if (type == "eoralist") {
-    exiolabels <- read.delim(paste0("Eora26_", min(year), "_bp/labels_T.txt"),
-                             header = F)
-    colnames <- c(paste0(exiolabels[[2]], "_", exiolabels[[4]]))
+    labels <- read.delim(paste0("Eora26_", min(year), "_bp/labels_T.txt"),
+                         header = F)
+
+    if (hasArg(country)) {
+      data <- lapply(data, function(x) {x[which(labels$V2 %in% country),
+                                          which(labels$V2 %in% country)]})
+      labels <- labels[which(labels$V2 %in% country),]
+    }
+
+    colnames <- c(paste0(labels[[2]], "_", labels[[4]]))
     data <- lapply(data[min(year):max(year)],
                    function(x) {colnames(x) <- colnames; x})
     data <- lapply(data, function(x) {row.names(x) <- colnames; x})
@@ -61,28 +76,44 @@ dyads <- function(year, data) {
     data <- lapply(data, function(x) {tidyr::separate(x, Var2,
                                                       into = c("C2", "S2"), sep = "_")})
 
+    # data <- lapply(data, function(x) {x[order(x$C1),]})
     data <- Map(cbind, year = as.list(year), data)
     final <- data.table::rbindlist(data)
 
   }
 
   if (type == "exiomatrix") {
-    exiolabels <- read.delim(paste0("IOT_", min(year), "_ixi/unit.txt"))
-    colnames(data) <- c(paste0(exiolabels[[1]], "_", exiolabels[[2]])); rm(exiolabels)
+    labels <- read.delim(paste0("IOT_", min(year), "_ixi/unit.txt"), header = F)
+    colnames(labels)[1] <- "V2"
+
+    if (hasArg(country)) {
+      data <- data[which(labels$V2 %in% country), which(labels$V2 %in% country)]
+      labels <- labels[which(labels$V2 %in% country),]
+    }
+
+    colnames(data) <- c(paste0(labels[[1]], "_", labels[[2]])); rm(labels)
     row.names(data) <- colnames(data)
 
     data <- reshape2::melt(data, as.is = T); gc() # as.is makes chr
     data <- tidyr::separate(data, Var1, into = c("C1", "S1"), sep = "_"); gc()
     data <- tidyr::separate(data, Var2, into = c("C2", "S2"), sep = "_"); gc()
 
-
+    # data <- data[order(data$C1),]
     final <- cbind(min(year), data); rm(data); gc()
 
   }
 
   if (type == "exiolist") {
-    exiolabels <- read.delim(paste0("IOT_", min(year), "_ixi/unit.txt"))
-    colnames <- c(paste0(exiolabels[[1]], "_", exiolabels[[2]])); rm(exiolabels)
+    labels <- read.delim(paste0("IOT_", min(year), "_ixi/unit.txt"))
+    colnames(labels)[1] <- "V2"
+
+    if (hasArg(country)) {
+      data <- lapply(data, function(x) {x[which(labels$V2 %in% country),
+                                          which(labels$V2 %in% country)]})
+      labels <- labels[which(labels$V2 %in% country),]
+    }
+
+    colnames <- c(paste0(labels[[1]], "_", labels[[2]])); rm(labels)
     data <- lapply(data[min(year):max(year)],
                    function(x) {colnames(x) <- colnames; x})
     data <- lapply(data, function(x) {row.names(x) <- colnames; x}); rm(colnames); gc()
@@ -94,6 +125,7 @@ dyads <- function(year, data) {
     data <- lapply(data, function(x) {tidyr::separate(x, Var2,
                                                       into = c("C2", "S2"), sep = "_")}); gc()
 
+    # data <- lapply(data, function(x) {x[order(x$C1),]})
     data <- Map(cbind, year = as.list(year), data); gc()
     final <- data.table::rbindlist(data); rm(data); gc()
 
