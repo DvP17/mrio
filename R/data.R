7c271ae40193ddb96b3d2a343ad12f2946dec1ba
DvP17 <dvp17@protonmail.ch>
2019-04-18 16:22:37 +0200

    added data characterization factors for EXIOBASE on climate change

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Data for Characterization Factors on Climate Change
+#'
+#' This is data on the characterization factors for climate change.
+#'
+#'
+#' @format A data frame with ...
+#'
+#'
+"cf_exio"
