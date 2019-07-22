2187d403b159e17765eb70d381f8fb8928fb251c
Presberger  David <davidpre@ethz.ch>
2019-07-22 14:29:56 +0200

    bug fix in exio

--- data-raw/cf_exio_multi.R
+++ data-raw/cf_exio_multi.R
@@ -7,7 +7,7 @@ cf_ws <- data.table::fread("C:/Data/Matlab Tool/Files/CF/CF_water.txt",
 cf_bl <- NULL
 
 cf_ws <- cf_ws[2:nrow(cf_ws), c(2 + c(1:8, rep(9, 11), rep(10, 144)))]
-cf_ws <- unlist(cf_ws)
+cf_ws <- as.numeric(unlist(t(cf_ws)))
 
 
 
